e067b665e173aed996a5918923d699e111ba9ef8
Alan da Silva Alves <linuxfama@gmail.com>
2021-06-19 01:12:44 -0300

    TDD - Form Registration Page with validades

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+# class User
 class User < ApplicationRecord
+  EMAIL_FORMAT = /[^\s]@[^\s]/.freeze
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, format: { with: EMAIL_FORMAT }, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
-# require "test_helper"
-#
-# class UserTest < ActiveSupport::TestCase
-#   # test "the truth" do
-#   #   assert true
-#   # end
-# end
+require 'test_helper'
+
+# class UserTest
+class UserTest < ActiveSupport::TestCase
+  test 'nome é obrigatório' do
+    user = User.new(name: nil)
+
+    user.save
+
+    assert user.errors[:name].any?
+  end
+
+  test 'email é obrigatório e no formato válido' do
+    user = User.new(email: nil)
+    user.save
+
+    assert user.errors[:email].any?
+
+    user = User.new(email: 'invalido')
+    user.save
+
+    assert user.errors[:email].any?
+
+    user = User.new(email: 'valido@exemplo.com')
+    user.save
+
+    refute user.errors[:email].any?
+  end
+
+  test 'email é único' do
+    user = User.create!(
+      name: 'Darth Vader',
+      email: 'varder@exemplo.com',
+      password: 'secret',
+      password_confirmation: 'secret'
+    )
+
+    other_user = User.create(email: 'varder@exemplo.com')
+
+    assert other_user.errors[:email].any?
+  end
+end
